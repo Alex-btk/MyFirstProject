127d6cf31f332901a335d8a0c395379e94978f63
Alex B <alex.batkov9@gmail.com>
2022-07-26 11:52:51 +0300

    modified app

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 
-const server = http.createServer((request, response) => { 
-    const url = request.url;
-    const method = request.method;
-    const a = 0;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
 
-    if (url === '/') {
-        response.write('<html>');
-        response.write('<head><title>Enter message</title></head>');
-        response.write('<body>');
-        response.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-        response.write('</body>');
-        response.write('</html>');
-        return response.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        response.statusCode = 302;
-        response.setHeader('Location', '/');
-        return response.end();
-    }
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<html>');
-    response.write('<head><title>My first Page</title></head>');
-    response.write('<body>');
-    response.write('<h1>just some text</h1>');
-    response.write('</body>');
-    response.write('</html>');
-    response.end();
-    // process.exit;
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-server.listen(3000);
+app.listen(3000);
